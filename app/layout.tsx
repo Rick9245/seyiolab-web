eaafd66974f8c505cb5b3ff843263464a860a47d
Rick9245 <sey2@free.fr>
2026-06-04 15:56:01 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     template: "%s — Seyio Lab",
   },
 verification: {
-  google: "ZAWvkQWkyi1o0YJA",
+  google: "ZAWvkQWkyi1o0",
 },
   description: siteDescription,
   keywords: [
